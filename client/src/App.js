67411a70dbaef66d5bbe24d3daadce88b3eae84f
teddysmithdev <teddysmithdev@gmail.com>
2019-11-14 15:31:38 -0500

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,91 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-class App extends Component {
-  render() {
+
+export default class App extends Component {
+  state = {
+    address: '',
+    cityState: '',
+    insurance: '',
+    value: '',
+    zpid: '',
+    tax: '',
+    rent: [],
+
+  }
+
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+
+  submitHandler = e => {
+    e.preventDefault()
+    axios
+    .get(`/api?address=${this.state.address}&cityState=${this.state.cityState}`)
+    .then(data => {
+      this.setState({ value: data.data.zestimate, rent: data.data.rentZest, zpid: data.data.zpid })
+    }).catch(err => console.log(err))
+  
+      fetch(`https://www.zillow.com/graphql/?zpid=${this.state.zpid}&operationName=PriceTaxQuery`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({query:"query PriceTaxQuery($zpid: ID!) {\n  property(zpid: $zpid) {\n    zpid\n    livingArea\n    countyFIPS\n    parcelId\n    taxHistory {\n      time\n      taxPaid\n      taxIncreaseRate\n      value\n      valueIncreaseRate\n    }\n    priceHistory {\n      time\n      price\n      priceChangeRate\n      event\n      source\n      buyerAgent {\n        photo {\n          url\n        }\n        profileUrl\n        name\n      }\n      sellerAgent {\n        photo {\n          url\n        }\n        profileUrl\n        name\n      }\n      showCountyLink\n      postingIsRental\n    }\n    currency\n    country\n  }\n}\n","operationName":"PriceTaxQuery","variables":{"zpid":54293120},"clientVersion":"home-details/5.47.0.166.master.e87315b"}),
+        })
+        .then(res => res.json())
+        .then(res => this.setState({tax: res.data.property.taxHistory[0].taxPaid}));
+      }     
+
+ 
+  render() { 
+    const { address, cityState, insurance } = this.state;
+    const NOI = (this.state.rent*12)-(this.state.tax)-(this.state.insurance)
+    const CAP = this.state.value / NOI
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Express Starter</h1>
-        </header>
-        <Customers />
+      <div>
+      <div classname="overlay"></div>
+      <div className="container">
+      <form onSubmit={this.submitHandler}>
+        <div class="form-group mt-3">
+        <label for="exampleInputEmail1"><h4>Address</h4></label>
+            <input
+              type="text"
+              name="address"
+              value={address}
+              onChange={this.onChange}
+              class="form-control"
+            />
+        </div>
+        <div class="form-group">
+        <label for="exampleInputPassword1"><h4>City & State</h4></label>
+            <input
+              type="text"
+              name="cityState"
+              value={cityState}
+              onChange={this.onChange}
+              class="form-control"
+            />
+          </div>
+          <div class="form-group">
+          <label for="exampleInputPassword1"><h4>Insurance</h4></label>
+            <input
+              type="text"
+              name="insurance"
+              value={insurance}
+              onChange={this.onChange}
+              class="form-control"
+            />
+            </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
+          </form>
+          <h3>Approximate Value: {this.state.value}</h3>
+          <h3>Rent: {this.state.rent}</h3>
+          <h3>Taxes: {this.state.tax}</h3>
+          <h5>Estimate Yearly NOI: {NOI}</h5>
+          <h5>Estimated CAP Rate: {CAP}</h5>
+          </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
--- server.js
+++ server.js
@@ -1,17 +1,62 @@
 const express = require('express');
-
+const cheerio = require('cheerio');
+const request = require('request');
+const cors = require('cors')
 const app = express();
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+//http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz17jhsapuqyz_7oq0o&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA
 
-  res.json(customers);
-});
+app.get('/api', cors(), (req, res) => {
 
-const port = 5000;
+    const options = {
+    //https://www.google.com/search?q=elijah+privette+704
+    url: `http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz17jhsapuqyz_7oq0o&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA&rentzestimate=true`,
+    headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+            }
+        };
 
-app.listen(port, () => `Server running on port ${port}`);
+    request(options, function (error, response, body) {
+        if(!error && response.statusCode == 200) {
+            let $ = cheerio.load(body, { xmlMode: true });
+            var zestimate = $('zestimate').find('amount').text()
+            var rentZest =  ($('rentzestimate').find('amount').text())*12
+            var zpid = $('zpid').text()
+            console.log(zestimate, rentZest, zpid) 
+            findTaxes(zpid)
+            } else {
+              console.log('connection error')
+            }
+      
+        })
+    });
+
+const findTaxes = (zpid) => {
+  const options = {
+    //https://www.google.com/search?q=elijah+privette+704
+    url: `https://www.zillow.com/homedetails/${zpid}_zpid`,
+    headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+            }
+      };
+
+      request(options, function (error, response, body) {
+        if(!error && response.statusCode == 200) {
+            let $ = cheerio.load(body);
+            var taxes = $('.zsg-tab-panel zsg-tab_active').text()
+            console.log(taxes) 
+            } else {
+              console.log('connection error')
+            }
+      
+        })
+
+}
+
+
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log('Server running...')
+})
