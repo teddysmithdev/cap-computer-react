426aad735c05f857bce2b5db0c77cc6bc1cb34c6
teddysmithdev <teddysmithdev@gmail.com>
2019-11-14 15:35:43 -0500

    second deploy

--- server.js
+++ server.js
@@ -9,52 +9,22 @@ const app = express();
 app.get('/api', cors(), (req, res) => {
 
     const options = {
-    //https://www.google.com/search?q=elijah+privette+704
-    url: `http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz17jhsapuqyz_7oq0o&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA&rentzestimate=true`,
-    headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-            }
-        };
+    url: `http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=X1-ZWz17jhsapuqyz_7oq0o&address=${req.query.address}&citystatezip=${req.query.cityState}&rentzestimate=true`,
+    };
 
     request(options, function (error, response, body) {
-        if(!error && response.statusCode == 200) {
-            let $ = cheerio.load(body, { xmlMode: true });
-            var zestimate = $('zestimate').find('amount').text()
-            var rentZest =  ($('rentzestimate').find('amount').text())*12
-            var zpid = $('zpid').text()
-            console.log(zestimate, rentZest, zpid) 
-            findTaxes(zpid)
-            } else {
-              console.log('connection error')
-            }
-      
-        })
-    });
-
-const findTaxes = (zpid) => {
-  const options = {
-    //https://www.google.com/search?q=elijah+privette+704
-    url: `https://www.zillow.com/homedetails/${zpid}_zpid`,
-    headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-            }
-      };
-
-      request(options, function (error, response, body) {
-        if(!error && response.statusCode == 200) {
             let $ = cheerio.load(body);
-            var taxes = $('.zsg-tab-panel zsg-tab_active').text()
-            console.log(taxes) 
-            } else {
-              console.log('connection error')
-            }
-      
-        })
+            var zestimate = $('zestimate').first().find('amount').text()
+            var rentZest =  ($('rentzestimate').find('amount').first().text())
+            var zpid = $('zpid').first().text()
 
-}
-
-
-
+            res.json({
+              zestimate: zestimate,
+              rentZest: rentZest,
+              zpid: zpid
+            })
+          })
+  })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
